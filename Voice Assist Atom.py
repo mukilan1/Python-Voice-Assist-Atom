0ba6618695fa41b7fd6b8ddb0f5882e175cafc8d
Mukilan <74693526+mukilan1@users.noreply.github.com>
2023-04-13 19:33:42 +0530

    Update Voice Assist Atom.py

--- Voice Assist Atom.py
+++ Voice Assist Atom.py
@@ -4,11 +4,6 @@ import datetime
 import wikipedia
 import webbrowser
 import random
-import socket
-
-server_address = ('192.168.166.154', 8080)
-
-server_esp32 = ("192.168.166.142",12345)
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
@@ -43,12 +38,7 @@ def take_command():
         query = None
     return query
 
-intro = """Hello there, I am a state-of-the-art robot designed for the modern age! My creators, Mukilan, Tharun, and Hari, have equipped me with the latest technology and communication capabilities using web Sockets. I can move around on four wheels and even rotate my head for added flexibility.
-
-My programming is based on Python and Micropython, and I can act as both a server and client communication system with my ESP32 and ESP8266 capabilities. My features are quite impressive, if I do say so myself. I can detect faces, act as a voice assistant and voice controlling system, rotate my head, move around like a robot, open voice software, search for things, control devices with my voice, and even be remotely controlled.
-
-What's more, I have a custom-designed BMS that makes me all the more unique. It's an honor to introduce myself to you and show off my many capabilities . And finally my name is Atom"""
-
+intro = "<your intro here>"
 grt = ["hai","hello", "hi", "hey"]
 
 if __name__ == "__main__":
@@ -147,89 +137,6 @@ if __name__ == "__main__":
             except:
                 speak("Sorry, I could not find information on that topic.")
                 
-        elif "turn on light " in query or "turn off light" in query:
-
-            try:
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect(server_address)
-
-                client_socket.send(query.encode())
-                response = client_socket.recv(1024).decode()
-
-                print(response)
-
-                client_socket.close()
-            except:
-                speak("sorry some thing went wrong ")
-                print("sorry some thing went wrong !")
-                
-       
-
-        elif "turn on all"  in query or "turn off all" in query:
-            try:
-                client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect(server_address)
-
-                client_socket.send(query.encode())
-                response = client_socket.recv(1024).decode()
-
-                print(response)
-
-                client_socket.close()
-            except:
-                speak("sorry some thing went wrong ")
-                print("sorry some thing went wrong !")
-
-       
-
-        elif "see up" in query or "look up" in query:
-                    try:
-                
-                        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        client_socket.connect(server_esp32)
-
-                        client_socket.send("48".encode())                        
-
-                        client_socket.close()
-
-                    except:
-                        speak("sorry some thing went wrong ")
-                        print("sorry some thing went wrong !")
-
-                
-
-        elif "see straight" in query or "look straight" in query:
-
-                    try:
-                
-                        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        client_socket.connect(server_esp32)
-
-                        client_socket.send("75".encode())
-                     
-                        client_socket.close()
-
-                    except:
-                        speak("sorry some thing went wrong ")
-                        print("sorry some thing went wrong !")
-                    
-                    
-        elif "see down" in query or "look down" in query:
-
-                    try:
-                
-                        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        client_socket.connect(server_esp32)
-
-                        client_socket.send("98".encode())
-                        client_socket.close()
-
-                    except:
-                        speak("sorry some thing went wrong ")
-                        print("sorry some thing went wrong !")
-                
-                
-           
 
         else:
                   speak("Sorry, I could not understand.")      
